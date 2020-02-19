7ecf9a50215c7149ce162e5835fa826d418c80a2
push0012 <pushpamal0012@gmail.com>
2020-02-19 14:13:46 +0530

    changes

--- app/Http/Controllers/PrometheusController.php
+++ app/Http/Controllers/PrometheusController.php
@@ -9,21 +9,40 @@ class PrometheusController extends Controller
 {
     public function index()
     {
-        $array = [
-            'job_name'=> 'node exporter metrics',
-                ['scrape_interval' => '5s',
-                    ['static_configs'=>[
-                        'targets'=> 'thehell'
+        
+                  /* [ 'crape_interval' => '5s',
+                        'static_configs'=> [
+                            'targets'=> 'thehell'
+                        ]
                     ]
-                ]
-                ]     
-        ];
+                ];*/
 
-           
-            $yaml = Yaml::dump($array);
-            file_put_contents('config.yaml', $yaml."\n", FILE_APPEND);
+/*
+        $addr = array(
+            "job_name" => "node exporter metrics",
+            "family"=> "Dumars",
+            "address"=> array(
+                "lines"=> "458 Walkman Dr.
+                Suite #292",
+                "city"=> "Royal Oak",
+                "state"=> "MI",
+                "postal"=> 48046,
+              ),
+          );
+*/
 
-                
+           /* $array = array(
+                "products"=>
+                array (
+                    array(
+                        'job_name'=> 'node exporter metrics'  
+                    )
+                )
+            );
+
+            $yaml = Yaml::dump($array,4,4);
+            file_put_contents('config.yaml', $yaml, FILE_APPEND);
+         */
     }
 }
 
