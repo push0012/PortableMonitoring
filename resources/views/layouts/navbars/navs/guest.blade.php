8520f076cc73e78e143e53b5e0e78e8a8ea2cacf
push0012 <pushpamal0012@gmail.com>
2020-03-03 15:58:45 +0530

    create dashboard

---
+++ resources/views/layouts/navbars/navs/guest.blade.php
@@ -0,0 +1,43 @@
+<nav class="navbar navbar-expand-lg navbar-absolute navbar-transparent fixed-top">
+    <div class="container-fluid">
+        <div class="navbar-wrapper">
+            <div class="navbar-toggle d-inline">
+                <button type="button" class="navbar-toggler">
+                    <span class="navbar-toggler-bar bar1"></span>
+                    <span class="navbar-toggler-bar bar2"></span>
+                    <span class="navbar-toggler-bar bar3"></span>
+                </button>
+               
+            </div>
+            <a class="navbar-brand ml-auto" style="padding-top:30px !important; padding-left:50px !important;" href="#">
+                
+                    <img style="width:100px !important;" src="{{ asset('img/head.png') }}" alt="{{ __('Profile Photo') }}">
+                
+            </a>
+        </div>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-bar navbar-kebab"></span>
+            <span class="navbar-toggler-bar navbar-kebab"></span>
+            <span class="navbar-toggler-bar navbar-kebab"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navigation">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a href="#" class="nav-link text-primary">
+                        <i class="tim-icons icon-laptop"></i> {{ __('Portable Server Monitoring Tool') }}
+                    </a>
+                </li>
+                <!--<li class="nav-item ">
+                    <a href="#" class="nav-link">
+                        <i class="tim-icons icon-laptop"></i> {{ __('Link 2') }}
+                    </a>
+                </li>
+                <li class="nav-item ">
+                    <a href="#" class="nav-link">
+                        <i class="tim-icons icon-single-02"></i> {{ __('Link 3') }}
+                    </a>
+                </li>-->
+            </ul>
+        </div>
+    </div>
+</nav>
--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,8 @@
 |
 */
 
-/*Route::get('/', function () {
-    return view('welcome');
-});*/
+Route::get('/', function () {
+    return view('dashboard');
+});
 
-Route::get('prom','PrometheusController@index');
+//Route::get('prom','PrometheusController@index');
