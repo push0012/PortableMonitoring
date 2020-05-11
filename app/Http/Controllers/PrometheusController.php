b5c1a323cdc5f90d3b42b2058a1ce599fd45d71c
push0012 <pushpamal0012@gmail.com>
2020-05-11 09:28:03 +0530

    do some changes

--- app/Http/Controllers/PrometheusController.php
+++ app/Http/Controllers/PrometheusController.php
@@ -15,46 +15,19 @@ class PrometheusController extends Controller
                             'targets'=> 'thehell'
                         ]
                     ];
-                
-/*
-        $addr = array(
-            "job_name" => "node exporter metrics",
-            "family"=> "Dumars",
-            "address"=> array(
-                "lines"=> "458 Walkman Dr.
-                Suite #292",
-                "city"=> "Royal Oak",
-                "state"=> "MI",
-                "postal"=> 48046,
-              ),
-          );
-*/
 
-           /* $array = array(
-                "products"=>
-                array (
-                    array(
-                        'job_name'=> 'node exporter metrics'  
-                    )
-                )
-            );
-*/
             $yaml = Yaml::dump($array,4,4);
             file_put_contents('config.yaml', $yaml, FILE_APPEND);/*
          */
     }
 }
 
-/*datasources:
-  - name: gdev-graphite
-    type: graphite
-    access: proxy
-    url: http://localhost:8180
-    jsonData:
-      graphiteVersion: "1.1"
-
+/*
+     
       FILE_APPEND
 - job_name: 'node_exporter_metrics'
     scrape_interval: 5s
         static_configs:
-        - targets: ['172.16.155.60:9100']*/
+        - targets: ['172.16.155.60:9100']
+        
+        */
