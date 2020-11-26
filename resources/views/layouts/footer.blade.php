c4ee9c48323462f1fdd5b43804335b06a580d9f5
push0012 <pushpamal0012@gmail.com>
2020-11-26 14:41:12 +0530

    change footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -14,8 +14,8 @@
         </ul>
         <div class="copyright">
             &copy; {{ now()->year }} {{ __('made with') }} <i class="tim-icons icon-heart-2"></i> {{ __('by') }}
-            <a href="https://creative-tim.com" target="_blank">{{ __('Pushpamal Gunasena') }}</a> &amp;
-            <a href="https://updivision.com" target="_blank">{{ __('Sandaru Rangana') }}</a> {{ __('for a better network') }}.
+            <a href="www.linkedln.com" target="_blank">{{ __('Pushpamal Gunasena') }}</a> &amp;
+            <a href="www.linkedln.com" target="_blank">{{ __('Sandaru Rangana') }}</a> {{ __('for a better network') }}.
         </div>
     </div>
 </footer>
