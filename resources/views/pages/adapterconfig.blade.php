f0ad90d2b8e74cc541805b42ef47a0b367ad404c
push0012 <pushpamal0012@gmail.com>
2020-12-02 11:25:00 +0530

    add id, remove form, add button pass

--- resources/views/pages/adapterconfig.blade.php
+++ resources/views/pages/adapterconfig.blade.php
@@ -14,30 +14,28 @@
                     <br><br>
                     <h3 class="title">{{ __('Adapter Configuration') }}</h3>
                 </div>
-                <form method="post" action="" autocomplete="off">
+                
                     <div class="card-body">
                             @csrf
-                            @method('put')
-
                             <div class="form-group">
                                 <label>{{ __('Host Name') }}</label>
-                                <input required type="text" name="host_name" class="form-control" placeholder="{{ __('Host Name') }}" value="">
+                                <input required type="text" name="host_name" id="host_name" class="form-control" placeholder="{{ __('Host Name') }}" value="">
                             </div>
 
                             <div class="form-group">
                                 <label>{{ __('IP Address') }}</label>
-                                <input required type="text" minlength="7" maxlength="15" size="15" pattern="^((\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.){3}(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])$" name="ip_address" class="form-control" placeholder="{{ __('xxx.xxx.xxx.xxx') }}" value="">
+                                <input required type="text" minlength="7" maxlength="15" size="15" pattern="^((\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.){3}(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])$" name="ip_address" id="ip_address" class="form-control" placeholder="{{ __('xxx.xxx.xxx.xxx') }}" value="">
                             </div>
 
                             <div class="form-group">
                                 <label>{{ __('Subnet Mask') }}</label>
-                                <input required type="text" minlength="7" maxlength="15" size="15" pattern="^((\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.){3}(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])$" name="subnet_mask" class="form-control" placeholder="{{ __('xxx.xxx.xxx.xxx') }}" value="">
+                                <input required type="text" minlength="7" maxlength="15" size="15" pattern="^((\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.){3}(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])$" name="subnet_mask" id="subnet_mask" class="form-control" placeholder="{{ __('xxx.xxx.xxx.xxx') }}" value="">
                             </div>
                     </div>
                     <div class="card-footer">
-                        <button type="submit" class="btn btn-fill btn-primary">{{ __('Save') }}</button>
+                        <button id="adapt_store" class="btn btn-fill btn-primary">{{ __('Save') }}</button>
                     </div>
-                </form>
+                
             </div>
         </div>
     </div>
