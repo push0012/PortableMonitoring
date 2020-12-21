2da8d992b90e5455d2243b671ebf019280451254
push0012 <pushpamal0012@gmail.com>
2020-12-21 11:50:07 +0530

    update config.ymal file creating data

--- app/Http/Controllers/PrometheusController.php
+++ app/Http/Controllers/PrometheusController.php
@@ -9,6 +9,24 @@ class PrometheusController extends Controller
 {
     public function index(Request $requests)
     {
+        file_put_contents('D:\config.yaml', "global:\n");
+        file_put_contents('D:\config.yaml', "\tscrape_interval:\t\t\t15s\n",FILE_APPEND);
+        file_put_contents('D:\config.yaml', "\tevaluation_interval:\t15s\n\n",FILE_APPEND);
+
+        file_put_contents('D:\config.yaml', "\texternal_labels:\n",FILE_APPEND);
+        file_put_contents('D:\config.yaml', "\t\t\tmonitor: "."'"."example"."'"." \n\n",FILE_APPEND);
+
+        file_put_contents('D:\config.yaml', "alerting:\n",FILE_APPEND);   
+        file_put_contents('D:\config.yaml', "\talertmanagers:\n",FILE_APPEND);
+        file_put_contents('D:\config.yaml', "\t- static_configs:\n",FILE_APPEND);
+        file_put_contents('D:\config.yaml', "\t\t- targets: ['localhost:9093']"."\n\n",FILE_APPEND);
+
+        file_put_contents('D:\config.yaml', "rule_files:\n",FILE_APPEND);  
+        file_put_contents('D:\config.yaml', "\t# - "."\""."first_rules.yml"."\""."\n",FILE_APPEND);
+        file_put_contents('D:\config.yaml', "\t# - "."\""."second_rules.yml"."\""."\n\n",FILE_APPEND);
+
+        file_put_contents('D:\config.yaml', "scrape_configs:\n",FILE_APPEND);  
+
         $parameter = json_decode($requests->getContent());
         // return response()->json($requests->getContent(), 200);
         foreach ($parameter as $request){ 
@@ -30,12 +48,17 @@ class PrometheusController extends Controller
         $json = array ();
         $outData = array ();
         $jsona;
+
+
+        
+        
+
         foreach ($inData as $value) {
 
         file_put_contents('D:\config.yaml', "\t- job_name: '".$value['job_name']."'"."\n",FILE_APPEND);
-        file_put_contents('D:\config.yaml', "\t\t\tscrape_interval: ".$value['interval']."s\n",FILE_APPEND);
-        file_put_contents('D:\config.yaml', "\t\t\t\t\tstatic_configs:\n",FILE_APPEND);
-        file_put_contents('D:\config.yaml', "\t\t\t\t\t- targets: ['".$value['target']."']"."\n\n",FILE_APPEND);
+        file_put_contents('D:\config.yaml', "\t\tscrape_interval: ".$value['interval']."s\n",FILE_APPEND);
+        file_put_contents('D:\config.yaml', "\t\tstatic_configs:\n",FILE_APPEND);
+        file_put_contents('D:\config.yaml', "\t\t\t- targets: ['".$value['target']."']"."\n\n",FILE_APPEND);
             /*$entry =   array(
                 'job_name' => $value['job_name'],
                 'scrape_interval' => $value['interval'],
